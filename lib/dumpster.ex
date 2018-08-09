e8e9115495b3961907ce5507afd56b20a3a7893b
elridion <hans@goedeke.org>
2018-08-09 10:50:54 +0200

    Added child_spec

--- lib/dumpster.ex
+++ lib/dumpster.ex
@@ -10,4 +10,14 @@ defmodule Dumpster do
   def dump(target \\ __MODULE__, payload) when is_binary(payload) do
     GenServer.cast(target, {:dump, payload})
   end
+
+  def child_spec(opts) do
+    %{
+      id: opts[:name] || __MODULE__,
+      start: {__MODULE__, :start_link, opts},
+      type: :worker,
+      restart: opts[:restart] || :permanent,
+      shutdown: opts[:shutdown] || 500
+    }
+  end
 end
